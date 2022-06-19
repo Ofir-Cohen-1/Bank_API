7bc2b9afec356bd672f94d1875ac8c1d82ef0a15
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-06-19 20:38:33 +0300

    Project initialization

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("Server running on port" + PORT);
+});
